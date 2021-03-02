9f99ecdd068997c6d8c76d5ace34834e026895d3
Siddhant Mishra <siddhant48311@gmail.com>
2021-03-03 02:13:48 +0530

    Binding done in RegisterActivity and activity_register.xml optimised.

--- app/src/main/java/com/example/properprepapp/RegisterActivity.java
+++ app/src/main/java/com/example/properprepapp/RegisterActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.properprepapp.databinding.ActivityRegisterBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
@@ -18,9 +19,10 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
-    private EditText emailText, passwordText, confirmPasswordText;
-    private Button submitSignupButton;
+
+    ActivityRegisterBinding binding;
     private FirebaseAuth mAuth; //declared firebase instance
+
 
     public void onLogin(View view) {
         finish();
@@ -30,52 +32,31 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        emailText = findViewById(R.id.emailTextSingup);
-        passwordText = findViewById(R.id.passwordTextSignup);
-        confirmPasswordText = findViewById(R.id.confirmPasswordTextSignup);
-        submitSignupButton = findViewById(R.id.submitSignupButton);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
         mAuth = FirebaseAuth.getInstance(); //initialized firebase instance
-        submitSignupButton.setOnClickListener(new View.OnClickListener() {
+        binding.submitSignupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createUser();
+                String emailText, passwordText;
+                emailText = binding.emailTextSignup.getText().toString();
+                passwordText = binding.passwordTextSignup.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(emailText, passwordText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Registered Successfully.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
 
-    }
-
-    // function to create a user
-    private void createUser() {
-        String email = emailText.getText().toString();
-        String password = passwordText.getText().toString();
-        String confirmPassword = confirmPasswordText.getText().toString();
-        if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.equals(confirmPassword)) {
-            if (!password.isEmpty()) {
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                Toast.makeText(RegisterActivity.this, "SignUp Successful!", Toast.LENGTH_SHORT).show();
-                                finish();
-                                startActivity(new Intent(RegisterActivity.this, SignoutActivity.class));
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(RegisterActivity.this, "Registration failed.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            } else {
-                passwordText.setError("Empty fields are not allowed.");
-            }
-        } else if (email.isEmpty()) {
-            emailText.setError("Empty fields are not allowed.");
-        } else if (!password.equals(confirmPassword)) {
-            confirmPasswordText.setError("Password does not match Confirm password");
-        } else {
-            emailText.setError("Please enter correct email.");
-        }
     }
 
 
