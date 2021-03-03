48f3d698094aff748553645c625f14e1d5342d5b
Siddhant Mishra <siddhant48311@gmail.com>
2021-03-03 15:28:25 +0530

    Splash screen created. Firestore integrated, user info storage successfull. (No bugs.)

--- app/src/main/java/com/example/properprepapp/LoginActivity.java
+++ app/src/main/java/com/example/properprepapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.properprepapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -11,14 +12,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.properprepapp.databinding.ActivityLoginBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText emailText, passwordText;
-    private Button submitLoginButton;
+
+    ActivityLoginBinding binding;
+    ProgressDialog progressDialog;
     private FirebaseAuth mAuth; // declared firebase instance
 
     public void onRegister(View view){
@@ -28,12 +31,14 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        emailText = findViewById(R.id.emailTextLogin);
-        passwordText = findViewById(R.id.passwordTextLogin);
-        submitLoginButton = findViewById(R.id.submitLoginButton);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         mAuth = FirebaseAuth.getInstance(); // initialized firebase instance
-        submitLoginButton.setOnClickListener(new View.OnClickListener() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Logging in...");
+
+        binding.submitLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkUser();
@@ -42,29 +47,32 @@ public class LoginActivity extends AppCompatActivity {
     }
     //function to check the user login
     private void checkUser(){
-        String email = emailText.getText().toString();
-        String password = passwordText.getText().toString();
+        String email = binding.emailTextLogin.getText().toString();
+        String password = binding.passwordTextLogin.getText().toString();
         if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
             if(!password.isEmpty()){
+                progressDialog.show();
                 mAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
                     public void onSuccess(AuthResult authResult) {
+                        progressDialog.dismiss();
                         Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(LoginActivity.this, MainActivity.class));
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
                         Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                     }
                 });
             }else{
-                passwordText.setError("Empty fields are not allowed.");
+                binding.passwordTextLogin.setError("Empty fields are not allowed.");
             }
         }else if(email.isEmpty()){
-            emailText.setError("Empty fields are not allowed.");
+            binding.emailTextLogin.setError("Empty fields are not allowed.");
         }else {
-            emailText.setError("Please enter correct email.");
+            binding.emailTextLogin.setError("Please enter correct email.");
         }
     }
 }
--- app/src/main/java/com/example/properprepapp/RegisterActivity.java
+++ app/src/main/java/com/example/properprepapp/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.properprepapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -17,11 +18,14 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterActivity extends AppCompatActivity {
 
     ActivityRegisterBinding binding;
-    private FirebaseAuth mAuth; //declared firebase instance
+    ProgressDialog progressDialog;
+    private FirebaseAuth mAuth; //declared firebase auth instance
+    private FirebaseFirestore db; //declared firestore instance
 
 
     public void onLogin(View view) {
@@ -35,21 +39,45 @@ public class RegisterActivity extends AppCompatActivity {
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
         mAuth = FirebaseAuth.getInstance(); //initialized firebase instance
+        db = FirebaseFirestore.getInstance(); //initialized firestore instance
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Creating new account...");
+
         binding.submitSignupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String emailText, passwordText;
+                String emailText, passwordText, nameText;
                 emailText = binding.emailTextSignup.getText().toString();
                 passwordText = binding.passwordTextSignup.getText().toString();
+                nameText = binding.nameViewSignup.getText().toString();
 
+                User user = new User(nameText, emailText, passwordText);
+                progressDialog.show();
                 mAuth.createUserWithEmailAndPassword(emailText, passwordText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(RegisterActivity.this, "Registered Successfully.", Toast.LENGTH_SHORT).show();
+                            String uid = task.getResult().getUser().getUid();
+                            db
+                                    .collection("users")
+                                    .document(uid)
+                                    .set(user)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                progressDialog.dismiss();
+                                                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                                                Toast.makeText(RegisterActivity.this, "Registered Successfully.", Toast.LENGTH_SHORT).show();
+                                            }else {
+                                                Toast.makeText(RegisterActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
                         } else {
+                            progressDialog.dismiss();
                             Toast.makeText(RegisterActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
